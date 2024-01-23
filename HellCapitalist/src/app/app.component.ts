96d3981c5bd903c88a688dd1d5e8129305349746
Louis Barbier <louis.barbier@etud.univ-jfc.fr>
2024-01-23 18:00:34 +0100

    Ajout Bouton !!!

--- HellCapitalist/src/app/app.component.ts
+++ HellCapitalist/src/app/app.component.ts
@@ -12,6 +12,46 @@ import { ProduitService } from '../car/ProduitService';
 })
 export class AppComponent {
   title = 'Hell Capitalist';
+
+  display1 = 'none';
+  display2 = 'none';
+  display3 = 'none';
+
+  openPage(opt:number) {
+    if(opt == 1){
+      this.display1 = 'block';
+      this.display2 = 'none';
+      this.display3 = 'none';
+    }
+    if(opt == 2){
+      this.display1 = 'none';
+      this.display2 = 'block';
+      this.display3 = 'none';
+    }
+    if(opt == 3){
+      this.display1 = 'none';
+      this.display2 = 'none';
+      this.display3 = 'block';
+    }
+  }
+
+  closePage() {
+    this.display1 = 'none';
+    this.display2 = 'none';
+    this.display3 = 'none';
+  }
+
+  i=0;
+  mults=["x1","x10","x100","MAX","NEXT"];
+
+  onClick() {
+    if (this.i < 4){
+      this.i = this.i + 1;
+    } else {
+      this.i = 0;
+    }
+  }
+
   produits;
 
   produitService = inject(ProduitService);
