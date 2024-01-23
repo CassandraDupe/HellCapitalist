934753f183152dde283bd99c1a54bc26dfdcde3a
CassandraDupe <cassandra.dupe@etud.univ-jfc.fr>
2024-01-23 17:48:15 +0100

    Service GraphQL complet

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,21 +1,23 @@
 const express = require('express');
-const { ApolloServer, gql } = require("apollo-server-express");
+const { ApolloServer } = require("apollo-server-express");
 
 // Construct a schema, using GraphQL schema language
-const typeDefs = gql`
- type Query {
- hello: String
- }
-`;
-// Provide resolver functions for your schema fields
-const resolvers = {
-    Query: {
-        hello: () => 'Hello world!',
-    },
-};
 
-const server = new ApolloServer({ typeDefs, resolvers})
+// Provide resolver functions for your schema fields
+const resolvers = require("./src/resolvers.js")
+const typeDefs = require("./src/schema.js")
+//Ajout du monde
+let world = require("./src/world.js")
+
+const server = new ApolloServer({
+    typeDefs, resolvers,
+    context: async ({ req }) => ({
+        world: world
+    })
+})
+
 const app = express();
+app.use(express.static('public'));
 server.start().then( res => {
     server.applyMiddleware({app});
     app.listen({port: 4000}, () =>
