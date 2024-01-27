e86b1821dd72a4d37e2accc98f44e4c9619347a7
Louis Barbier <louis.barbier@etud.univ-jfc.fr>
2024-01-27 22:41:56 +0100

    Valeurs de produit "justes"

--- HellCapitalist/src/app/produit.component.ts
+++ HellCapitalist/src/app/produit.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Product } from '../class/World';
 
@@ -10,7 +10,33 @@ import { Product } from '../class/World';
   styleUrl: './produit.component.css'
 })
 export class ProduitComponent {
-    @Input() prod = new Product;
+  product!: Product;
+  seu: number | undefined; // Prochain seuil (undefined si il n'y a pas de prochain seuil)
+  valeur!: string | undefined;
+  multiplicateur!: string;
+  totMoney!: number;
+  achat = -1;
 
-    valeur = "billion"
+  @Input()
+  set params(value: any) {
+    this.product = value.prod!;
+    this.seu = this.product.palliers.find(elem => elem.unlocked == false)?.seuil;
+    this.valeur = "billion";
+    this.multiplicateur = value.mult!;
+    this.totMoney = value.money!;
+    if(this.multiplicateur.startsWith('x')){
+      this.achat = +this.multiplicateur.substring(1);
+    }
+    if(this.multiplicateur == "NEXT"){
+      if(typeof(this.seu) != undefined){
+        this.achat = this.seu! - this.product.quantite;
+      }
+      else{
+        this.achat = Math.floor(this.totMoney/this.product.cout); // A CHANGER !!! NE PREND PAS EN COMPTE LA CROISSANCE
+      }
+    }
+    if(this.multiplicateur == "MAX"){
+      this.achat = Math.floor(this.totMoney/this.product.cout); // A CHANGER !!! NE PREND PAS EN COMPTE LA CROISSANCE 
+    }
   }
+}
