bd66769b94ec870e1dcccd3b46da81ca2e8a4279
CassandraDupe <cassandra.dupe@etud.univ-jfc.fr>
2024-03-29 15:38:53 +0100

    Paramètres des objets personnalisés

--- BackEnd/src/world.js
+++ BackEnd/src/world.js
@@ -44,11 +44,11 @@ module.exports = {
             "id": 2,
             "name": "Dog toys",
             "logo": "../icones/dog_toys.png",
-            "cout": 16,
+            "cout": 20,
             "croissance": 1.2,
-            "revenu": 7,
+            "revenu": 9,
             "vitesse": 3000,
-            "quantite": 1,
+            "quantite": 0,
             "timeleft": 0,
             "managerUnlocked": false,
             "paliers": [
@@ -58,7 +58,7 @@ module.exports = {
                     "seuil": 25,
                     "idcible": 2,
                     "ratio": 2,
-                    "typeratio": "vitesse",
+                    "typeratio": "gain",
                     "unlocked": false
                 },
                 {
@@ -66,8 +66,8 @@ module.exports = {
                     "logo": "../icones/dog_toys.png",
                     "seuil": 50,
                     "idcible": 2,
-                    "ratio": 2,
-                    "typeratio": "gain",
+                    "ratio": 3,
+                    "typeratio": "vitesse",
                     "unlocked": false
                 }
             ]
@@ -76,11 +76,11 @@ module.exports = {
             "id": 3,
             "name": "Scream and despair",
             "logo": "../icones/scream_and_despair.png",
-            "cout": 16,
-            "croissance": 1.2,
-            "revenu": 7,
-            "vitesse": 3000,
-            "quantite": 1,
+            "cout": 100,
+            "croissance": 1.5,
+            "revenu": 45,
+            "vitesse": 25000,
+            "quantite": 0,
             "timeleft": 0,
             "managerUnlocked": false,
             "paliers": [
@@ -88,8 +88,8 @@ module.exports = {
                     "name": "Premier palier",
                     "logo": "../icones/scream_and_despair.png",
                     "seuil": 25,
-                    "idcible": 2,
-                    "ratio": 2,
+                    "idcible": 3,
+                    "ratio": 3,
                     "typeratio": "vitesse",
                     "unlocked": false
                 },
@@ -97,9 +97,9 @@ module.exports = {
                     "name": "Deuxième palier",
                     "logo": "../icones/scream_and_despair.png",
                     "seuil": 50,
-                    "idcible": 2,
-                    "ratio": 2,
-                    "typeratio": "gain",
+                    "idcible": 3,
+                    "ratio": 2.5,
+                    "typeratio": "vitesse",
                     "unlocked": false
                 }
             ]
@@ -108,11 +108,11 @@ module.exports = {
             "id": 4,
             "name": "Flesh and bone",
             "logo": "../icones/flesh_and_bone.png",
-            "cout": 16,
-            "croissance": 1.2,
-            "revenu": 7,
-            "vitesse": 3000,
-            "quantite": 1,
+            "cout": 500,
+            "croissance": 2.1,
+            "revenu": 270,
+            "vitesse": 100000,
+            "quantite": 0,
             "timeleft": 0,
             "managerUnlocked": false,
             "paliers": [
@@ -120,18 +120,18 @@ module.exports = {
                     "name": "Premier palier",
                     "logo": "../icones/flesh_and_bone.png",
                     "seuil": 25,
-                    "idcible": 2,
-                    "ratio": 2,
-                    "typeratio": "vitesse",
+                    "idcible": 4,
+                    "ratio": 3.2,
+                    "typeratio": "gain",
                     "unlocked": false
                 },
                 {
                     "name": "Deuxième palier",
                     "logo": "../icones/flesh_and_bone.png",
                     "seuil": 50,
-                    "idcible": 2,
-                    "ratio": 2,
-                    "typeratio": "gain",
+                    "idcible": 4,
+                    "ratio": 4,
+                    "typeratio": "vitesse",
                     "unlocked": false
                 }
             ]
@@ -140,11 +140,11 @@ module.exports = {
             "id": 5,
             "name": "Torture machine",
             "logo": "../icones/torture_machine.png",
-            "cout": 16,
-            "croissance": 1.2,
-            "revenu": 7,
-            "vitesse": 3000,
-            "quantite": 1,
+            "cout": 3000,
+            "croissance": 2.8,
+            "revenu": 1300,
+            "vitesse": 4500000,
+            "quantite": 0,
             "timeleft": 0,
             "managerUnlocked": false,
             "paliers": [
@@ -152,8 +152,8 @@ module.exports = {
                     "name": "Premier palier",
                     "logo": "../icones/torture_machine.png",
                     "seuil": 25,
-                    "idcible": 2,
-                    "ratio": 2,
+                    "idcible": 5,
+                    "ratio": 6,
                     "typeratio": "vitesse",
                     "unlocked": false
                 },
@@ -161,8 +161,8 @@ module.exports = {
                     "name": "Deuxième palier",
                     "logo": "../icones/torture_machine.png",
                     "seuil": 50,
-                    "idcible": 2,
-                    "ratio": 2,
+                    "idcible": 5,
+                    "ratio": 4,
                     "typeratio": "gain",
                     "unlocked": false
                 }
@@ -172,11 +172,11 @@ module.exports = {
             "id": 6,
             "name": "Maths lessons",
             "logo": "../icones/maths_lessons.png",
-            "cout": 16,
-            "croissance": 1.2,
-            "revenu": 7,
-            "vitesse": 3000,
-            "quantite": 1,
+            "cout": 10000,
+            "croissance": 5,
+            "revenu": 800,
+            "vitesse": 12000000,
+            "quantite": 0,
             "timeleft": 0,
             "managerUnlocked": false,
             "paliers": [
@@ -184,18 +184,18 @@ module.exports = {
                     "name": "Premier palier",
                     "logo": "../icones/maths_lessons.png",
                     "seuil": 25,
-                    "idcible": 2,
-                    "ratio": 2,
-                    "typeratio": "vitesse",
+                    "idcible": 6,
+                    "ratio": 5,
+                    "typeratio": "gain",
                     "unlocked": false
                 },
                 {
                     "name": "Deuxième palier",
                     "logo": "../icones/maths_lessons.png",
                     "seuil": 50,
-                    "idcible": 2,
-                    "ratio": 2,
-                    "typeratio": "gain",
+                    "idcible": 6,
+                    "ratio": 8,
+                    "typeratio": "vitesse",
                     "unlocked": false
                 }
             ]
